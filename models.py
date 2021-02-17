a8e86362d2f710d8c9b0348056a9935cf154b70f
RobJiggs <72582495+RobJiggs@users.noreply.github.com>
2021-02-16 20:10:55 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,17 @@
+from datetime import datetime
+from mentor import db, login_manager
+from flask_login import UserMixin
+
+
 @login_manager.user_loader
 def load_user(user_id):
     user=User.query.filter(id==user_id).first()
-    if user == '':
-        user=Usermentor.query.filter(id==user_id).first()
-        return Usermentor.query.get(int(user_id))
-    else:
+    if user != '':
         return User.query.get(int(user_id))
+    else:
+
+        user = Usermentor.query.filter(id == user_id).first()
+        return Usermentor.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
@@ -36,4 +42,24 @@ class Usermentor(db.Model, UserMixin):
     mentee = db.relationship('User', backref='link', lazy=True)
 
     def __repr__(self):
-        return f"UserMentor('{self.username}','{self.experience}', '{self.email}','{self.mentee}, '{self.image_file}')"
+        return f"UserMentor('{self.username}','{self.status}','{self.experience}', '{self.email}','{self.mentee}, '{self.image_file}')"
+
+class Jobfield(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='defaultfield.jpg')
+    jtitles= db.relationship('Jobtitle', backref='Job', lazy=True)
+
+    def __repr__(self):
+        return f"Jobfield('{self.name}','{self.image_file}')"
+
+
+class Jobtitle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='defaultjob.jpg')
+    jobfield_id= db.Column(db.Integer, db.ForeignKey('jobfield.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Jobtitle('{self.name}', '{self.image_file}',)"
+
